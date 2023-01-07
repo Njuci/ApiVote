5ccfe5e2dab292af10ab48b7da9c16b19638c340
Biringanine Destin <113047575+DestinBir@users.noreply.github.com>
2023-01-07 22:28:55 +0200

    Revert "Add files via upload"

--- VoteApp/SimulationEtudiant/views.py
+++ VoteApp/SimulationEtudiant/views.py
@@ -4,23 +4,21 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Faculte,promotion,Etudiant
 
-
 class FaculteDispo(APIView):
     def get(self,request,format=None):
         facDispo=Faculte.objects.all()
         serializer=FaculteSerial(facDispo,many=True)
         return Response(serializer.data)
-
-
 class PromotionDispo(APIView):
     def get(self,request,format=None):
         prmDisp=promotion.objects.all()
         serializer=PromotionSerializer(prmDisp,many=True)
         return Response(serializer.data)
-
-
 class EtudiantDisp(APIView):
     def get(self,request,format=None):
         etudiantDisp=Etudiant.objects.all()
         serializer=EtudiantSerial(etudiantDisp,many=True)
         return Response(serializer.data)
+
+    
+        
